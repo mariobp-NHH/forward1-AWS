4005787e41685b3200e2f23b571aeb87f925128c
mariobp-NHH <78204796+mariobp-NHH@users.noreply.github.com>
2022-11-30 16:59:41 +0100

    Update utils.py

--- webse/users/utils.py
+++ webse/users/utils.py
@@ -2,7 +2,44 @@ import secrets
 import os
 from PIL import Image
 from webse import application
+import boto3
+from botocore.exceptions import NoCredentialsError
+import base64
+import io
+import os
+import secrets
 
+# # For the GitHub:
+# BUCKET="forward-v1"
+
+
+# def read_image(image_filename):
+#     im = Image.open(Bucket(BUCKET).Object(image_filename).get().get('Body'))
+#     data = io.BytesIO()
+#     im.save(data, im.format)
+#     encoded_img_data = base64.b64encode(data.getvalue())
+#     return encoded_img_data.decode('utf-8')
+
+
+# def save_picture(form_picture):
+#     random_hex = secrets.token_hex(8)
+#     _, f_ext = os.path.splitext(form_picture.filename)
+#     picture_fn = random_hex + f_ext
+
+#     output_size = (125, 125)
+#     i = Image.open(form_picture)
+#     i.thumbnail(output_size)
+
+#     in_mem_file = io.BytesIO()
+#     i.save(in_mem_file, format=i.format)
+
+#     in_mem_file.seek(0)
+
+#     Bucket(BUCKET).put_object(Key=picture_fn, Body=in_mem_file)
+
+#     return picture_fn
+
+#For the computer original
 def save_picture(form_picture):
     random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
@@ -16,33 +53,12 @@ def save_picture(form_picture):
 
     return picture_fn
 
+# For the computer bucket s3:
+# ACCESS_KEY="AKIAU5ZFCCSCH3KFDZGM"
 
-# from flask_wtf import FlaskForm
-# from flask_wtf.file import FileField, FileAllowed
-# from flask_login import current_user
-# from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField, SelectField, RadioField, FloatField
-# from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError, InputRequired, Optional
-# from PIL import Image
-# import boto3
-# from botocore.exceptions import NoCredentialsError
-# import base64
-# import io
-# import os
-# import secrets
+# SECRET_KEY="cbh9vX9Ms0wdDbaa5C360QeB2o/MJmL/WVdqDxAU"
 
-###-------Bucket Info
-
-#ACCESS_KEY=os.environ.get('ACCESS_KEY', None)
-
-#SECRET_KEY=os.environ.get('SECRET_KEY', None)
-
-#BUCKET=os.environ.get('BUCKET', None)
-
-# ACCESS_KEY="AKIAUCJDCUAMV3T6RJMW"
-
-# SECRET_KEY="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"
-
-# BUCKET="ene425pics"
+# BUCKET="forward-v1"
 
 # s3_r = boto3.resource('s3',
 #                       aws_access_key_id=ACCESS_KEY,
