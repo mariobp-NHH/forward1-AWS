cc326f6a33c3dba0b77a26c437ac232b4598002d
mariobp-NHH <78204796+mariobp-NHH@users.noreply.github.com>
2022-12-08 08:48:56 +0100

    Delete webse directory

--- webse/users/forms.py
+++
@@ -1,3 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, SelectField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
