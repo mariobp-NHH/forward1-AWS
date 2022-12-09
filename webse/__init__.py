9448e694d12ccbc655c32b8cae723b310d8030e1
mariobp-NHH <78204796+mariobp-NHH@users.noreply.github.com>
2022-12-09 13:00:17 +0100

    Update __init__.py

--- webse/__init__.py
+++ webse/__init__.py
@@ -6,10 +6,10 @@ import os
 
 application = Flask(__name__)
 
+DBVAR = f"postgresql://{os.environ['RDS_USERNAME']}:{os.environ['RDS_PASSWORD']}@{os.environ['RDS_HOSTNAME']}/{os.environ['RDS_DB_NAME']}"
 application.config['SECRET_KEY'] = '1dfc4dedcdsdsd5b2ffa3a090dfc34f845fd'
-DBVAR = 'sqlite:///user.db'
-application.config['SQLALCHEMY_DATABASE_URI'] = DBVAR
-application.config['SQLALCHEMY_BINDS'] ={'gd_course': 'sqlite:///gd_course.db', 'gender_platform': 'sqlite:///gender_platform.db'}
+application.config['SQLALCHEMY_DATABASE_URI'] = DBVAR 
+application.config['SQLALCHEMY_BINDS'] ={'gd_course': DBVAR , 'gender_platform': DBVAR}
 db = SQLAlchemy(application)
 bcrypt = Bcrypt(application)
 login_manager= LoginManager(application)
